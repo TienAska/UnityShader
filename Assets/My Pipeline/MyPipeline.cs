9f0478ac857d12d2d8930db33c748f790eb18a8f
AskaTien <constantine.sky@hotmail.com>
2019-07-30 01:28:08 +0800

    [Update] Complete custom render pipeline

--- Assets/My Pipeline/MyPipeline.cs
+++ Assets/My Pipeline/MyPipeline.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.Rendering;
+using Conditional = System.Diagnostics.ConditionalAttribute;
 
 public class MyPipeline : RenderPipeline
 {
@@ -12,37 +13,87 @@ public class MyPipeline : RenderPipeline
         }
     }
 
+
+    CommandBuffer cameraBuffer = new CommandBuffer() { name = "Render Camera" };
+
+    CullingResults cull;
+
     void Render(ScriptableRenderContext context, Camera camera)
     {
+        // culling parameter setup
         ScriptableCullingParameters cullingParameters;
         if (!camera.TryGetCullingParameters(out cullingParameters))
         {
             return;
         }
-        CullingResults cull = context.Cull(ref cullingParameters);
 
+#if UNITY_EDITOR
+        // inject ui into scene window
+        if (camera.cameraType == CameraType.SceneView)
+        {
+            ScriptableRenderContext.EmitWorldGeometryForSceneView(camera);
+        }
+#endif
+
+        cull = context.Cull(ref cullingParameters);
+
+        // setup current camera
         context.SetupCameraProperties(camera);
 
-        var buffer = new CommandBuffer() { name = camera.name };
+        // camera clear
         CameraClearFlags clearFlags = camera.clearFlags;
-        buffer.ClearRenderTarget
+        cameraBuffer.ClearRenderTarget
         (
             (clearFlags & CameraClearFlags.Depth) != 0,
             (clearFlags & CameraClearFlags.Color) != 0,
             camera.backgroundColor
         );
-        context.ExecuteCommandBuffer(buffer);
-        buffer.Release();
+        cameraBuffer.BeginSample("Render Camera");
+        context.ExecuteCommandBuffer(cameraBuffer);
+        cameraBuffer.Clear();
+
+        // draw opaque
         var drawSettings = new DrawingSettings(new ShaderTagId("SRPDefaultUnlit"), new SortingSettings(camera));
         var filterSettings = FilteringSettings.defaultValue;
         filterSettings.renderQueueRange = RenderQueueRange.opaque;
         context.DrawRenderers(cull, ref drawSettings, ref filterSettings);
 
+        // draw skeybox
         context.DrawSkybox(camera);
 
+        // draw transparent
         filterSettings.renderQueueRange = RenderQueueRange.transparent;
         context.DrawRenderers(cull, ref drawSettings, ref filterSettings);
 
+        // draw defult surface shader
+        DrawDefaultPipeline(context, camera);
+
+        cameraBuffer.EndSample("Render Camera");
+        context.ExecuteCommandBuffer(cameraBuffer);
+        cameraBuffer.Clear();
+
         context.Submit();
+    }
+
+    Material errorMaterial;
+
+    [Conditional("DEVELOPMENT_BUILD"), Conditional("UNITY_EDITOR")]
+    void DrawDefaultPipeline(ScriptableRenderContext context, Camera camera)
+    {
+        if (errorMaterial == null)
+        {
+            Shader errorShader = Shader.Find("Hidden/InternalErrorShader");
+            errorMaterial = new Material(errorShader) { hideFlags = HideFlags.HideAndDontSave };
+        }
+
+        var drawSettings = new DrawingSettings(new ShaderTagId("ForwardBase"), new SortingSettings(camera)) { overrideMaterial = errorMaterial, overrideMaterialPassIndex = 0 };
+        drawSettings.SetShaderPassName(1, new ShaderTagId("PrepassBase"));
+        drawSettings.SetShaderPassName(2, new ShaderTagId("Always"));
+        drawSettings.SetShaderPassName(3, new ShaderTagId("Vertex"));
+        drawSettings.SetShaderPassName(4, new ShaderTagId("VertexLMRGBM"));
+        drawSettings.SetShaderPassName(5, new ShaderTagId("VertexLM"));
+        var filterSettings = FilteringSettings.defaultValue;
+
+        context.DrawRenderers(cull, ref drawSettings, ref filterSettings);
     }
 }
