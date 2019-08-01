c59c79d27319e0878b5b0f960bca25d066a1ab13
AskaTien <constantine.sky@hotmail.com>
2019-08-01 18:51:34 +0800

    [Add] Add a editor script
    1. 添加创建HLSL文件的脚本

---
+++ Assets/SRP/Editor/CreateCustomFile.cs
@@ -0,0 +1,36 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.IO;
+using System.Text;
+
+public class CreateCustomFile
+{
+    [MenuItem("Assets/Create/Shader/HLSL")]
+    static void CreateHLSLFile()
+    {
+        CreateFile("HLSL");
+    }
+
+    static void CreateFile(string fileEx)
+    {
+        var assetPath = AssetDatabase.GetAssetPath(Selection.activeObject);
+        var dataPath = Application.dataPath.Replace("Assets", "");
+        var FileName = "new" + fileEx + "." + fileEx.ToLower();
+        var assetName = assetPath + "/" + FileName;
+        var fullPath = dataPath + assetName;
+
+        for (int i = 1; File.Exists(fullPath); i++)
+        {
+            var newFileName = "new" + fileEx + i.ToString() + "." + fileEx.ToLower();
+            assetName = assetPath + "/" + newFileName;
+            fullPath = dataPath + assetName;
+        }
+
+        File.WriteAllText(fullPath, "", Encoding.UTF8);
+
+        AssetDatabase.Refresh();
+
+        var asset = AssetDatabase.LoadAssetAtPath(assetName, typeof(object));
+        Selection.activeObject = asset;
+    }
+}
