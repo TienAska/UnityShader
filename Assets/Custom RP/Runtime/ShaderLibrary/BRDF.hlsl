8f26c5b8b0212dc6ee795e1e1d6534841a20724b
AskaTien <constantine.sky@hotmail.com>
2020-01-06 02:01:24 +0800

    [Add] brdf

---
+++ Assets/Custom RP/Runtime/ShaderLibrary/BRDF.hlsl
@@ -0,0 +1,34 @@
+﻿#ifndef CUSTOM_BRDF_INCLUDED
+#define CUSTOM_BRDF_INCLUDED
+
+struct BRDF
+{
+	float3 diffuse;
+	float3 specular;
+	float roughness;
+};
+
+#define MIN_REFLECTIVITY 0.04
+
+float OneMinusReflectivity(float metallic)
+{
+	float range = 1.0 - MIN_REFLECTIVITY;
+	return range - metallic * range;
+}
+
+BRDF GetBRDF(Surface surface)
+{
+	BRDF brdf;
+	
+	float oneMinusReflectivity = OneMinusReflectivity(surface.metallic);
+	brdf.diffuse = surface.color * oneMinusReflectivity;
+	
+	brdf.specular = lerp(MIN_REFLECTIVITY, surface.color, surface.metallic);
+	
+	float perceptualRoughness = PerceptualSmoothnessToPerceptualRoughness(surface.smoothness);
+	brdf.roughness = PerceptualRoughnessToRoughness(perceptualRoughness);
+	
+	return brdf;
+}
+
+#endif // CUSTOM_BRDF_INCLUDED
